a14aa1a64df153d5edf7c654ef55f66cabd14b38
kin96neko <kin96neko@gmail.com>
2019-12-04 13:02:21 +0000

    c6 sinnkisakuseipe-zi made

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,21 @@
 class TasksController < ApplicationController
   
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+  
   def show
+    @task = Task.find(params[:id])
   end
 
   def index
